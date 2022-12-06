6bf6326753148599c5cca349122b11a7db022bc2
himanshu-199 <karn07rawat@gmail.com>
2022-12-06 16:38:44 +0530

    deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Home from "./containers/Home";
 import useCookiesStatus from "./useHookes";
